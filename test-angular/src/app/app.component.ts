979c40710e4036597537b31cbdeb60a10b0d8a3b
Nikita Ilyanov <nikita.glk@mail.ru>
2023-07-28 02:31:06 +0300

    feat: add table

--- test-angular/src/app/app.component.ts
+++ test-angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+import {NgFor} from '@angular/common';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Test-Angular';
+  data: any;
+
+  nameColumns = ['number', 'name', 'age', 'gender', 'position'];
+  rows = [
+    {
+      number: '1',
+      name: 'Eric',
+      age: '19',
+      gender: 'Male',
+      position: 'Front-end'
+    },
+    {
+      number: '2',
+      name: 'Alex',
+      age: '28',
+      gender: 'Male',
+      position: 'Designer'
+    },
+    {
+      number: '3',
+      name: 'Ben',
+      age: '18',
+      gender: 'Male',
+      position: 'Back-end'
+    },
+    {
+      number: '4',
+      name: 'Lina',
+      age: '23',
+      gender: 'Female',
+      position: 'Project Manager'
+    },
+    {
+      number: '5',
+      name: 'Ricky',
+      age: '30',
+      gender: 'Male',
+      position: 'Product manager'
+    }
+   ];
+
+  columns: any[] = [
+    {
+      name: 'number',
+      title: '№'
+    },
+    {
+      name: 'name',
+      title: 'Name'
+    },
+    {
+      name: 'age',
+      title: 'Age'
+    },
+    {
+      name: 'gender',
+      title: 'Gender'
+    },
+    {
+      name: 'position',
+      title: 'Position'
+    }
+  ];
+
+  ngOnInit() {
+    this.data = new MatTableDataSource(this.rows);
+  }
+
+  tableDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.nameColumns, event.previousIndex, event.currentIndex);
+  }
 }
--- test-angular/src/app/app.module.ts
+++ test-angular/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    CdkTableModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
